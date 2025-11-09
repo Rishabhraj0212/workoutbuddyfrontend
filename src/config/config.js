e6851c0ce4df57da155c08d5ea334f702aad0e0f
Rishabh Raj <101053075+Rishabhraj0212@users.noreply.github.com>
2025-11-09 13:55:32 +0530

    Update API_URL for development configuration

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    API_URL: 'https://workoutbuddybackend-0kov.onrender.com'  // for development
-    // API_URL: 'https://your-production-url.com'  // for production - uncomment and change this when deploying
+    API_URL: 'https://workoutbuddybackend-0kov.onrender.com' 
+    
 };
 
 export default config;
